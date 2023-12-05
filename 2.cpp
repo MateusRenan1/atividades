2dd170bab418466535447ee0a664c22316926ac5
Mateus Renan <152920112+MateusRenan1@users.noreply.github.com>
2023-12-05 17:54:42 -0300

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,11 +1,11 @@
 int main() {
-  int num1, num2, soma;
+  int num1, num2;
   printf("digite o primeiro numero: ");
-  scanf("%d", &num1);
+  scanf("%i", &num1);
   printf("digite o segundo numero: ");
-  scanf("%d", &num2);
-  soma = num1 + num2;
-   printf("A soma dos números %d e %d é: %d\n", num1, num2, soma);
+  scanf("%i", &num2);
+  int soma = num1 + num2;
+   printf("A soma dos números é: %i", num1, num2, soma);
   
   return 0;
 }
